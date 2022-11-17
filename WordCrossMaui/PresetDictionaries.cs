26825eb8600ec45c30bf84e54bdf8a250330462c
Hidenobu Yamada <hyamada0307@gmail.com>
2022-11-18 01:00:16 +0900

    プリセット追加

--- WordCrossMaui/PresetDictionaries.cs
+++ WordCrossMaui/PresetDictionaries.cs
@@ -18,6 +18,7 @@ namespace WordCrossMaui
                     new DictionaryInfo("Collins English", "https://www.collinsdictionary.com/dictionary/english/", "-", "", true),
                     new DictionaryInfo("Collins Thesaurus", "https://www.collinsdictionary.com/dictionary/english-thesaurus/", "-", "", true),
                     new DictionaryInfo("Merriam Webster", "https://www.merriam-webster.com/dictionary/", "%20", "", true),
+                    new DictionaryInfo("American Heritage", "https://ahdictionary.com/word/search.html?q=", "+", "", false),
                     new DictionaryInfo("英辞郎", "https://eow.alc.co.jp/", "+", "", true),
                     new DictionaryInfo("Weblio英和・和英", "https://ejje.weblio.jp/content/", "+", "", true),
                     new DictionaryInfo("goo辞書英和・和英", "https://dictionary.goo.ne.jp/srch/en/", "+", "/m0u/", true),
